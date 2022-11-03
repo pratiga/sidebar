79abee186f067328d19db70db0ce3ba47efafe5a
pratiga <gaitrisanu2468@gmail.com>
2022-11-03 17:22:58 +0545

    sidebar data
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/data/data.jsx
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    id: "1",
+    title: "go wining",
+    details:
+      "Go Winning is a Gambling & Casinos company and has headquarters in Tiswadi, Goa. Go Winning has 1-10 employees.",
+    Numberofemployees: "11-50",
+    Location: "Mumbai, Maharasthra",
+    status: "completed",
+  },
+  {
+    id: "2",
+    title: "playwin lotto",
+    details:
+      "Playwin Lotto is a Gambling & Casinos company and has headquarters in Mumbai, Maharashtra. Playwin Lotto has 1-10 employees. Playwin Lotto was founded in 2002. Playwin Lotto is a privately held company.",
+    Numberofemployees: "11-50",
+    Location: "Mumbai, Maharasthra",
+    status: "completed",
+  },
+  {
+    id: "3",
+    title: "wozo gaming",
+    details:
+      "Wozo Gaming is a Gambling & Casinos company and has headquarters in New Delhi, New Delhi. Wozo Gaming has 11-50 employees.",
+    Location: "Panjim, Goa",
+    Numberofemployees: "11-50",
+    status: "notcompleted",
+  },
+  {
+    id: "3",
+    title: "satta reasults",
+    details:
+      "Satta Results is a Gambling & Casinos company and has headquarters in Mumbai, Maharastra. Satta Results has 1-10 employees.",
+    Numberofemployees: "11-50",
+    Location: "Noida, Uttar Pradesh",
+    status: "completed",
+  },
+  {
+    id: "4",
+    title: "rngplay",
+    details:
+      "Rngplay is a Gambling & Casinos company and has headquarters in Noida, Up. Rngplay has 1-10 employees.",
+    Numberofemployees: "11-50",
+    Location: "Noida, Uttar Pradesh",
+    status: "completed",
+  },
+  {
+    id: "5",
+    title: "Sattamatka5com",
+    details:
+      "Sattamatka5com is a Gambling & Casinos company and has headquarters in Mumbai, Maharasthra. Sattamatka5com has 1-10 employees. Sattamatka5com is a public company.",
+    Numberofemployees: "11-50",
+    Location: "Mumbai, Maharasthra",
+    status: "completed",
+  },
+  {
+    id: "6",
+    title: "Delta Corp Limited",
+    details:
+      "Delta Corp Limited is a Gambling & Casinos company and has headquarters in Maharashtra, Mumbai. Delta Corp Limited has 201-500 employees.",
+    Numberofemployees: "11-50",
+    Location: "Mumbai, Maharasthra",
+    status: "completed",
+  },
+];
