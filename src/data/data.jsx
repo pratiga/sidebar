6796d4bc903c8e03caa3a567dc871b429d50a656
pratiga <gaitrisanu2468@gmail.com>
2022-11-05 08:33:26 +0545

    update
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -2,8 +2,10 @@ export const data = [
   {
     id: "1",
     title: "go wining",
+    image:
+      "https://themerkle.com/wp-content/uploads-new/2017/05/shutterstock_342026228.jpg",
     details:
-      "Go Winning is a Gambling & Casinos company and has headquarters in Tiswadi, Goa. Go Winning has 1-10 employees.",
+      "Go Winning is a Gambling & Casinos company and has headquartersGames are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. in Tiswadi, Goa. Go Winning has 1-10 employees.",
     Numberofemployees: "11-50",
     Location: "Mumbai, Maharasthra",
     status: "completed",
@@ -11,8 +13,10 @@ export const data = [
   {
     id: "2",
     title: "playwin lotto",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4O37eRfMKylXIGUj-Wx7w6_NbO5Bjtwm1KA&usqp=CAU",
     details:
-      "Playwin Lotto is a Gambling & Casinos company and has headquarters in Mumbai, Maharashtra. Playwin Lotto has 1-10 employees. Playwin Lotto was founded in 2002. Playwin Lotto is a privately held company.",
+      "Playwin Lotto is a Gambling & Casinos company and has headquarters in Mumbai, Maharashtra.Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Playwin Lotto has 1-10 employees. Playwin Lotto was founded in 2002. Playwin Lotto is a privately held company.",
     Numberofemployees: "11-50",
     Location: "Mumbai, Maharasthra",
     status: "completed",
@@ -20,44 +24,53 @@ export const data = [
   {
     id: "3",
     title: "wozo gaming",
+    image:
+      "https://www.mountainflyermagazine.com/img/upimages/Gear/BIKES/2020/Kona_Wozo/BPR_0493.JPG",
     details:
-      "Wozo Gaming is a Gambling & Casinos company and has headquarters in New Delhi, New Delhi. Wozo Gaming has 11-50 employees.",
+      "Wozo Gaming is a Gambling & Casinos company and has headquarters in New Delhi, New Delhi.Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Wozo Gaming has 11-50 employees.",
     Location: "Panjim, Goa",
     Numberofemployees: "11-50",
     status: "notcompleted",
   },
   {
-    id: "3",
-    title: "satta reasults",
-    details:
-      "Satta Results is a Gambling & Casinos company and has headquarters in Mumbai, Maharastra. Satta Results has 1-10 employees.",
-    Numberofemployees: "11-50",
-    Location: "Noida, Uttar Pradesh",
-    status: "completed",
-  },
-  {
     id: "4",
-    title: "rngplay",
+    title: "satta reasults",
+    image:
+      "https://naijacampusjams.com/wp-content/uploads/2022/10/Munbae-vaa-en-anbae-vaaOonae-vaa-uyirae-vaa-2022-09-15T171336.497-1024x576.jpg",
     details:
-      "Rngplay is a Gambling & Casinos company and has headquarters in Noida, Up. Rngplay has 1-10 employees.",
+      "Satta Results is a Gambling & Casinos company and has headquarters in Mumbai, Maharastra. Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules.Satta Results has 1-10 employees.",
     Numberofemployees: "11-50",
     Location: "Noida, Uttar Pradesh",
     status: "completed",
   },
   {
     id: "5",
-    title: "Sattamatka5com",
+    title: "rngplay",
+    image:
+      "https://rngplay.com/sites/default/files/styles/banner_image/public/rgd_0.jpg?itok=x5ZGhkcI",
     details:
-      "Sattamatka5com is a Gambling & Casinos company and has headquarters in Mumbai, Maharasthra. Sattamatka5com has 1-10 employees. Sattamatka5com is a public company.",
+      "Rngplay is a Gambling & Casinos company and has headquarters in Noida, Up. Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Rngplay has 1-10 employees.",
+    Numberofemployees: "11-50",
+    Location: "Noida, Uttar Pradesh",
+    status: "completed",
+  },
+  {
+    id: "6",
+    title: "Sattamatka5com",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-COc7VTWbCEfjVlIa02z5liOZbtvbXf0-Vw&usqp=CAU",
+    details:
+      "Sattamatka5com is a Gambling & Casinos company and has headquarters in Mumbai, Maharasthra.Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Sattamatka5com has 1-10 employees. Sattamatka5com is a public company.",
     Numberofemployees: "11-50",
     Location: "Mumbai, Maharasthra",
     status: "completed",
   },
   {
-    id: "6",
+    id: "7",
     title: "Delta Corp Limited",
+    image: "https://cricketprediction.com/site/assets/files/2612/deltin.jpg",
     details:
-      "Delta Corp Limited is a Gambling & Casinos company and has headquarters in Maharashtra, Mumbai. Delta Corp Limited has 201-500 employees.",
+      "Delta Corp Limited is a Gambling & Casinos company and has headquarters in Maharashtra, Mumbai. Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Delta Corp Limited has 201-500 employees.",
     Numberofemployees: "11-50",
     Location: "Mumbai, Maharasthra",
     status: "completed",
