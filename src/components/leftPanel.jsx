bf74ec1606acf54ca681d05f7ac9e3aa056f1ada
pratiga <gaitrisanu2468@gmail.com>
2022-11-05 20:23:23 +0545

    leftpanel
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/leftPanel.jsx
+++ src/components/leftPanel.jsx
@@ -1,41 +1,30 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from "react";
+import React from "react";
 import { data } from "../data/data";
-import "../styles/sidebar.css";
-import RightSidebar from "./rightSidebar";
-const sidebar1 = () => {
-  const [detail, setDetail] = useState();
+
+const leftPanel = ({ detail }) => {
   let count = 0;
-  function handeldetails(item) {
-    setDetail({ ...item });
-  }
   data.map(() => count++);
   return (
-    <sidebar>
-      <div className="left-sidebar">
-        {detail ? (
-          <div className="leftPanel">
-            <h1 className="title"> {detail.title}</h1>
-            <div className="item-image">
-              <img src={detail.image} alt="images" />
-            </div>
-            <div className="detail-box">
-              <span>{detail.Location}</span>
-              <span>{detail.status}</span>
-            </div>
-            <p className="description">{detail.details}</p>
+    <>
+      {detail ? (
+        <div className="leftPanel">
+          <h1 className="title"> {detail.title}</h1>
+          <div className="item-image">
+            <img src={detail.image} alt="images" />
           </div>
-        ) : (
-          <div className="left-panel">
-            <h1>Total No of ITEM: {count}</h1>
+          <div className="detail-box">
+            <span>{detail.Location}</span>
+            <span>{detail.status}</span>
           </div>
-        )}
-      </div>
-      <div className="right-sidebar">
-        <RightSidebar handeldetails={handeldetails} />
-      </div>
-    </sidebar>
+          <p className="description">{detail.details}</p>
+        </div>
+      ) : (
+        <div className="left-panel">
+          <h1>Total No of ITEM: {count}</h1>
+        </div>
+      )}
+    </>
   );
 };
 
-export default sidebar1;
+export default leftPanel;
