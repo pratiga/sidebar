aca7cdfb351e162798eb46d5a3e0d483d2e58138
pratiga <gaitrisanu2468@gmail.com>
2022-11-09 16:10:16 +0545

    update
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -2,33 +2,20 @@
 import React, { useState } from "react";
 import RightSidebar from "./rightSidebar";
 import LeftPanel from "./leftPanel";
-import LoadmorePagination from "./loadmorePagination";
 
 const Sidebar = () => {
   const [detail, setDetail] = useState();
-  const [visible, setVisible] = useState(5);
-  const [isLoading, setIsLoading] = useState(true);
   function handeldetails(item) {
     setDetail({ ...item });
   }
-  const showMoreItem = () => {
-    setTimeout(() => {
-      setVisible((prevValue) => prevValue + 5);
-    }, 1000);
-    setIsLoading(false);
-  };
 
   return (
     <sidebar>
       <div className="left-sidebar">
         <LeftPanel detail={detail} />
-        <LoadmorePagination isLoading={isLoading} visible={visible} />
       </div>
       <div className="right-sidebar">
-        <RightSidebar
-          handeldetails={handeldetails}
-          showMoreItem={showMoreItem}
-        />
+        <RightSidebar handeldetails={handeldetails} />
       </div>
     </sidebar>
   );
--- src/components/rightSidebar.jsx
+++ src/components/rightSidebar.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
 import { data } from "../data/data";
 import "../styles/sidebar.css";
+import Cardloader from "./cardloader";
 
-const rightSidebar = ({ handeldetails, showMoreItem }) => {
+const rightSidebar = ({ handeldetails }) => {
+  const [visible, setVisible] = useState(3);
+  const [isLoading, setIsLoading] = useState(false);
+  const showMoreItem = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setVisible((prevValue) => prevValue + 5);
+      setIsLoading(false);
+    }, 1000);
+  };
   return (
     <div className="right-sidebar">
-      {data.map((item) => (
+      {data.slice(0, visible).map((item) => (
         <div className="items-box" onClick={() => handeldetails(item)}>
           <div className="items-image">
             <img src={item.image} alt="images" />
@@ -17,7 +28,8 @@ const rightSidebar = ({ handeldetails, showMoreItem }) => {
           </div>
         </div>
       ))}
-      <button className="btns loadmore" onClick={showMoreItem} >
+      {isLoading === true && <Cardloader />}
+      <button className="btns loadmore"  onClick={showMoreItem}>
         Load item
       </button>
     </div>
