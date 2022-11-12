5b09d5f9bdc97555e37d80f71520574226f8666a
pratiga <gaitrisanu2468@gmail.com>
2022-11-12 21:21:10 +0545

    list of items
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from "react";
+import "../styles/sidebar.css";
+import Cardloader from "./cardloader";
+
+const rightSidebar = ({
+  handeldetails,
+  FilterData,
+  showMoreItem,
+  isLoading,
+}) => {
+  return (
+    <div className="right-sidebar">
+      {FilterData.map((item) => (
+        <div className="items-box" onClick={() => handeldetails(item)}>
+          <div className="items-image">
+            <img src={item.image} alt="images" />
+          </div>
+          <div className="detail-box">
+            <h1 className="title">{item.title}</h1>
+            <span>{item.Location}</span>
+            <span>{item.status}</span>
+          </div>
+        </div>
+      ))}
+      {isLoading === true && <Cardloader />}
+      <button className="btns loadmore" onClick={showMoreItem}>
+        Load item
+      </button>
+    </div>
+  );
+};
+
+export default rightSidebar;
