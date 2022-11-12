da64a393c0a5525a9192a309a1c8f22e5c70d718
pratiga <gaitrisanu2468@gmail.com>
2022-11-12 21:20:52 +0545

    update
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,14 +1,27 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
-import RightSidebar from "./rightSidebar";
+import { data } from "../data/data";
+import ItemList from "./ItemList";
 import LeftPanel from "./leftPanel";
+import CustomerList from "./customerList";
 
-const Sidebar = () => {
+const Homepage = () => {
   const [detail, setDetail] = useState();
   const [visible, setVisible] = useState(3);
+  const [isLoading, setIsLoading] = useState();
+  const [active, setActive] = useState("SecondCard");
   function handeldetails(item) {
     setDetail({ ...item });
   }
+  function showMoreItem() {
+    setIsLoading(true);
+    setTimeout(() => {
+      setVisible((prevValue) => prevValue + 5);
+      setIsLoading(false);
+    }, 1000);
+  }
+
+  const FilterData = data.slice(0, visible);
 
   return (
     <sidebar>
@@ -16,13 +29,24 @@ const Sidebar = () => {
         <LeftPanel detail={detail} visible={visible} />
       </div>
       <div className="right-sidebar">
-        <RightSidebar handeldetails={handeldetails}
-          visible={visible}
-          setVisible={setVisible}
-         />
+        <nav>
+          <button onClick={() => setActive("SecondCard")}>ItemList</button>
+          <button onClick={() => setActive("Customer")}>CustomerList</button>
+        </nav>
+        <div>
+          {active === "SecondCard" && (
+            <ItemList
+              handeldetails={handeldetails}
+              FilterData={FilterData}
+              showMoreItem={showMoreItem}
+              isLoading={isLoading}
+            />
+          )}
+          {active === "Customer" && <CustomerList />}
+        </div>
       </div>
     </sidebar>
   );
 };
 
-export default Sidebar;
+export default Homepage;
