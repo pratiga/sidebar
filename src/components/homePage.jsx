5003895a3e3fd4a08a8458c4f098e53e608b0a7d
pratiga <gaitrisanu2468@gmail.com>
2022-11-14 10:16:49 +0545

    sort items
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
 import { data } from "../data/data";
@@ -7,11 +8,24 @@ import CustomerList from "./customerList";
 
 const Homepage = () => {
   const [detail, setDetail] = useState();
-  const [visible, setVisible] = useState(3);
+  const [items, setItems] = useState(data);
+  const [visible, setVisible] = useState(5);
   const [isLoading, setIsLoading] = useState();
   const [active, setActive] = useState("SecondCard");
+
   function handeldetails(item) {
     setDetail({ ...item });
+  }
+
+  function handleSort(e) {
+    const selectedElements = e.target.value;
+    if (selectedElements === "Ascending") {
+      const sortedData = [...items].sort((a, b) => a.id - b.id);
+      setItems(sortedData);
+    } else if (selectedElements === "Descending") {
+      const sortedData = [...items].sort((a, b) => b.id - a.id);
+      setItems(sortedData);
+    }
   }
   function showMoreItem() {
     setIsLoading(true);
@@ -20,8 +34,7 @@ const Homepage = () => {
       setIsLoading(false);
     }, 1000);
   }
-
-  const FilterData = data.slice(0, visible);
+  const filterData = items.slice(0, visible);
 
   return (
     <sidebar>
@@ -32,12 +45,18 @@ const Homepage = () => {
         <nav>
           <button onClick={() => setActive("SecondCard")}>ItemList</button>
           <button onClick={() => setActive("Customer")}>CustomerList</button>
+          <label>Sorted</label>
+          <select onChange={handleSort}>
+            <option defaultvalue="sort">sort</option>
+            <option value="Ascending">Ascending</option>
+            <option value="Descending">Descending</option>
+          </select>
         </nav>
         <div>
           {active === "SecondCard" && (
             <ItemList
               handeldetails={handeldetails}
-              FilterData={FilterData}
+              items={filterData}
               showMoreItem={showMoreItem}
               isLoading={isLoading}
             />
