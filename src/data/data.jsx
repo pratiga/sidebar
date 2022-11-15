77fc413359b397329c011b263e358405aa1558c1
pratiga <gaitrisanu2468@gmail.com>
2022-11-15 06:21:48 +0545

    data
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,7 +1,7 @@
 export const data = [
   {
-    id: "1",
-    title: "go wining",
+    id: "10",
+    title: "gowining",
     image:
       "https://themerkle.com/wp-content/uploads-new/2017/05/shutterstock_342026228.jpg",
     details:
@@ -12,7 +12,7 @@ export const data = [
   },
   {
     id: "2",
-    title: "playwin lotto",
+    title: "playwinlotto",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4O37eRfMKylXIGUj-Wx7w6_NbO5Bjtwm1KA&usqp=CAU",
     details:
@@ -23,7 +23,7 @@ export const data = [
   },
   {
     id: "3",
-    title: "wozo gaming",
+    title: "wozogaming",
     image:
       "https://www.mountainflyermagazine.com/img/upimages/Gear/BIKES/2020/Kona_Wozo/BPR_0493.JPG",
     details:
@@ -34,7 +34,7 @@ export const data = [
   },
   {
     id: "4",
-    title: "satta reasults",
+    title: "sattareasults",
     image:
       "https://naijacampusjams.com/wp-content/uploads/2022/10/Munbae-vaa-en-anbae-vaaOonae-vaa-uyirae-vaa-2022-09-15T171336.497-1024x576.jpg",
     details:
@@ -66,11 +66,33 @@ export const data = [
     status: "completed",
   },
   {
-    id: "7",
-    title: "Delta Corp Limited",
+    id: "17",
+    title: "DeltaCorpLimited",
     image: "https://cricketprediction.com/site/assets/files/2612/deltin.jpg",
     details:
       "Delta Corp Limited is a Gambling & Casinos company and has headquarters in Maharashtra, Mumbai. Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Delta Corp Limited has 201-500 employees.",
+    Numberofemployees: "11-50",
+    Location: "Mumbai, Maharasthra",
+    status: "completed",
+  },
+  {
+    id: "8",
+    title: "rngplay",
+    image:
+      "https://rngplay.com/sites/default/files/styles/banner_image/public/rgd_0.jpg?itok=x5ZGhkcI",
+    details:
+      "Rngplay is a Gambling & Casinos company and has headquarters in Noida, Up. Games are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. Rngplay has 1-10 employees.",
+    Numberofemployees: "11-50",
+    Location: "Noida, Uttar Pradesh",
+    status: "completed",
+  },
+  {
+    id: "9",
+    title: "gowining",
+    image:
+      "https://themerkle.com/wp-content/uploads-new/2017/05/shutterstock_342026228.jpg",
+    details:
+      "Go Winning is a Gambling & Casinos company and has headquartersGames are sometimes played purely for enjoyment, sometimes for achievement or reward as well. They can be played alone, in teams, or online; by amateurs or by professionals. The players may have an audience of non-players, such as when people are entertained by watching a chess championship. On the other hand, players in a game may constitute their own audience as they take their turn to play. Often, part of the entertainment for children playing a game is deciding who is part of their audience and who is a player. A toy and a game are not the same. Toys generally allow for unrestricted play whereas games come with present rules. in Tiswadi, Goa. Go Winning has 1-10 employees.",
     Numberofemployees: "11-50",
     Location: "Mumbai, Maharasthra",
     status: "completed",
